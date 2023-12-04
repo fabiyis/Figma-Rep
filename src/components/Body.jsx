86de7cbb7ac754679ea5948cef648abeb89bcc16
fabiyis <141014211+fabiyis@users.noreply.github.com>
2023-12-04 09:29:57 -0800

    modify contents in Body and Footer components

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -17,7 +17,7 @@ function Body() {
                 <div className="py-2 w-40 h-10 text-center text-white bg-blue-400 rounded-full mt-4 md:mt-8">Consult today</div>
             </div>
             <div className="md:w-1/2 mt-4 md:mt-0">
-                <img className="w-full h-auto" src={'../public/images/figma 1.png'} alt="Illustration" />
+                <img className="w-full h-auto" src={'../images/figma 1.png'} alt="Illustration" />
             </div> 
         </div>
         
@@ -31,7 +31,7 @@ function Body() {
         
         <div className="px-6 md:px-20 mt-6 md:mt-20 flex flex-wrap gap-6 justify-center">
             <div className=" w-80 h-80 bg-slate-200">
-                <img className=" w-24 h-24 mr-56 pt-10" src={'../public/images/serch icon.svg'} />
+                <img className=" w-24 h-24 mr-56 pt-10" src={'../images/serch icon.svg'} />
                 <div>
                     <h1 className="pb-5 font-bold text-2xl ml-4 mt-5">Search Doctor</h1>
                     <p className="ml-4 text-base h-7 w-72">Choose your doctor from thousands<br/>
@@ -41,7 +41,7 @@ function Body() {
             </div>
 
             <div className=" w-80 h-80 bg-slate-200">
-                <img className=" w-24 h-24 mr-56 pt-10" src={'../public/images/bottle.svg'} />
+                <img className=" w-24 h-24 mr-56 pt-10" src={'../images/bottle.svg'} />
                 <div>
                     <h1 className="pb-5 font-bold text-2xl ml-4 mt-5">Online pharmacy</h1>
                     <p className="ml-4 text-base h-7 w-72">Buy  your medicines with our <br/>
@@ -51,7 +51,7 @@ function Body() {
             </div>
 
             <div className=" w-80 h-80 bg-slate-200">
-                <img className=" w-24 h-24 mr-56 pt-10" src={'../public/images/contact.svg'} />
+                <img className=" w-24 h-24 mr-56 pt-10" src={'../images/contact.svg'} />
                 <div>
                     <h1 className="pb-5 font-bold text-2xl ml-4 mt-5">Consultation</h1>
                     <p className="ml-4 text-base h-7 w-72">Free consultation with our trusted<br/>
@@ -60,7 +60,7 @@ function Body() {
             </div>
 
             <div className=" w-80 h-80 bg-slate-200">
-                <img className=" w-24 h-24 mr-56 pt-10" src={'../public/images/details.svg'} />
+                <img className=" w-24 h-24 mr-56 pt-10" src={'../images/details.svg'} />
                 <div>
                     <h1 className="pb-5 font-bold text-2xl ml-4 mt-5">Details info</h1>
                     <p className="ml-4 text-base h-7 w-72">Free consultation with our trusted doctors and get the best recommendations
@@ -69,7 +69,7 @@ function Body() {
             </div>
 
             <div className=" w-80 h-80 bg-slate-200">
-                <img className=" w-24 h-24 mr-56 pt-10" src={'../public/images/emerg.svg'} />
+                <img className=" w-24 h-24 mr-56 pt-10" src={'../images/emerg.svg'} />
                 <div>
                     <h1 className="pb-5 font-bold text-2xl ml-4 mt-5">Emergency care</h1>
                     <p className="ml-4 text-base h-7 w-72">You can get 24/7 urgent care for <br/>
@@ -80,7 +80,7 @@ function Body() {
             </div>
 
             <div className=" w-80 h-80 bg-slate-200">
-                <img className=" w-24 h-24 mr-56 pt-10" src={'../public/images/track.svg'} />
+                <img className=" w-24 h-24 mr-56 pt-10" src={'../images/track.svg'} />
                 <div>
                     <h1 className="pb-5 font-bold text-2xl ml-4 mt-5">Tracking</h1>
                     <p className="ml-4 text-base h-7 w-72">Track and save your medical history <br/>
@@ -95,7 +95,7 @@ function Body() {
 
         <div className="flex flex-col-reverse md:flex-row items-center">
            <div className="md:w-1/2 md:mt-0">
-              <img className=" pl-10 w-full md:w-80 h-80 md:ml-20" src={'../public/images/trafalgar-illustration sec02 1.png'} alt="Illustration" />
+              <img className=" pl-10 w-full md:w-80 h-80 md:ml-20" src={'../images/trafalgar-illustration sec02 1.png'} alt="Illustration" />
            </div>
            <div className="text-center md:text-left md:ml-8">
               <h1 className="text-2xl md:text-4xl lg:text-5xl mb-4 md:mb-6">Leading healthcare<br/> providers</h1>
@@ -111,7 +111,7 @@ function Body() {
            </div>
         </div>
 
-        <div className="flex flex-col md:flex-row items-center mt-28">
+        <div className="flex flex-col-reverse md:flex-row items-center">
         <div className="md:w-1/2 mt-28 ml-36 text-2xl">
            <h> Download our <br/>mobile apps</h>
         <div>
@@ -121,24 +121,24 @@ function Body() {
           Our dedicated patient engagement app <br/>and web portal allow you to access<br/> information instantaneously (no <br/> tedious form, long calls, or <br/>administrative hassle) and securely
         </div>
         <div className="text-lg flex w-40 h-10 justify-center text-blue-400 rounded-full border ml-1 border-sky-600 hover:cursor-pointer mt-4 md:mt-6 lg:mt-8 mx-auto md:mb-0">
-          Download <img className="w-4 h-3 ml-1 mt-1" src="../public/images/arrow.svg" alt="arrow" />
+          Download <img className="w-4 h-3 ml-1 mt-1" src="../images/arrow.svg" alt="arrow" />
         </div>
         </div>
         <div>
-          <img className="w-full md:w-80 h-80 ml-10 mt-6 mr-96 md:ml-20" src={'../public/images/illu 03.png'} alt="Illustration" />
+          <img className="w-full md:w-80 h-80 ml-10 mt-6 mr-96 md:ml-20" src={'../images/illu 03.png'} alt="Illustration" />
        </div>
        </div>
 
-       <div className="flex justify-center items-center">
+       <div className="flex flex-col md:flex-row items-center mt-28">
             <div className="relative rounded-3xl h-80 bg-blue-400 mt-28 mx-20 px-96"> 
-              <img className="absolute top-0 right-0" src={'../public/images/Group.svg'} alt="" />
+              <img className="absolute top-0 right-0" src={'../images/Group.svg'} alt="" />
               <div className="flex flex-col items-center justify-center h-full">
                  <h1 className="text-2xl font-normal -mt-28 pt-52 text-white">What our customers are saying</h1>
                  <div className=" text-white">______</div>
 
                  <div className=" flex -ml-72 pt-10">
                     <div className="flex ">
-                       <img className="rounded-full r h-24 mb-10" src={'../public/images/img 1.png'} alt="" />
+                       <img className="rounded-full r h-24 mb-10" src={'../images/img 1.png'} alt="" />
                        <div className=" text-white mt-5 ml-5 "> 
                          <h1 className=" text-lg ">Edward Newgate</h1>
                          <p className=" text-xs ">Founder Circle</p>
@@ -172,7 +172,7 @@ function Body() {
 
         <div className="px-6 md:px-20 mt-6 md:mt-20 flex flex-wrap gap-6 justify-center">
             <div className=" w-80 h-80">
-                <img className=" w-fit h-60 mr-56  rounded-2xl" src={'../public/images/img f1.png'} />
+                <img className=" w-fit h-60 mr-56  rounded-2xl" src={'../images/img f1.png'} />
                 <div>
                     <h1 className="pb-5 font-bold text-2xl ml-4 mt-5">Disease detection, check 
                      up in the laboratory</h1>
@@ -181,12 +181,12 @@ function Body() {
                 </div>
                 <div className=" flex ml-4 mt-5">
                     <h1 className=" text-blue-600 font-bold text-sm cursor-pointer">Read more</h1>
-                    <img className=" ml-2" src={'../public/images/sA 1.svg'} />
+                    <img className=" ml-2" src={'../images/sA 1.svg'} />
                 </div>
             </div>
 
             <div className=" w-80 h-80">
-                <img className=" w-fit h-60 mr-56 rounded-2xl" src={'../public/images/img f2.png'} />
+                <img className=" w-fit h-60 mr-56 rounded-2xl" src={'../images/img f2.png'} />
                 <div>
                     <h1 className="pb-5 font-bold text-2xl ml-4 mt-5">Herbal medicines that are 
                      safe for consumption</h1>
@@ -194,12 +194,12 @@ function Body() {
                 </div>
                 <div className=" flex ml-4 mt-5">
                     <h1 className=" text-blue-600 font-bold text-sm cursor-pointer">Read more</h1>
-                    <img className=" ml-2" src={'../public/images/sA 1.svg'} />
+                    <img className=" ml-2" src={'../images/sA 1.svg'} />
                 </div>
             </div>
 
             <div className=" w-80 h-80 ">
-                <img className=" w-fit h-60 mr-56 rounded-2xl" src={'../public/images/img f3.png'} />
+                <img className=" w-fit h-60 mr-56 rounded-2xl" src={'../images/img f3.png'} />
                 <div>
                     <h1 className="pb-5 font-bold text-2xl ml-4 mt-5">Natural care for healthy 
                      facial skin</h1>
@@ -208,12 +208,12 @@ function Body() {
                 </div>
                 <div className=" flex ml-4 mt-5">
                     <h1 className=" text-blue-600 font-bold text-sm cursor-pointer">Read more</h1>
-                    <img className=" ml-2" src={'../public/images/sA 1.svg'} />
+                    <img className=" ml-2" src={'../images/sA 1.svg'} />
                 </div>
             </div>
         </div>
 
-        <div className=" flex justify-center items-center h-screen">
+        <div className=" flex justify-center items-center h-screen -mt-20 -mb-36">
             <div className=" text-lg w-28  text-center text-blue-400 rounded-full border  border-sky-600 hover:cursor-pointer ">
                <h1>View all</h1>
             </div>
--- src/components/MyFooter.jsx
+++ src/components/MyFooter.jsx
@@ -6,18 +6,18 @@ function MyFooter() {
     <Footer container className='bg-blue-400'>
       <div className="w-full">
         <div className=" justify-between sm:flex sm:justify-between md:flex md:grid-cols-1">
-          <div className=' flex justify-center pl-40'>
+          <div className=' flex justify-center pl-32'>
             <div className='mr-3 h-6 w-10 sm:h-9 bg-white rounded-full flex justify-center items-center'>
               <h1 className='text-blue-400 font-bold h-6 '>T</h1>
             </div>
             <div>
                <p className=" font-semibold text-white mt-1">Trafalgar</p>
             </div>
-            <div className=' mt-14 -ml-28'>
-               <p className=' text-white text-xs'> Trafalgar provides progressive, and<br/>affordable healthcare, accessible on<br/> mobile and online 
-               for everyone</p>
-               <div className=' mt-8'>
-                 <p className=' text-xs text-white'>©Trafalgar PTY LTD 2020. All rights reserved </p>
+            <div className='mt-8 sm:mt-14 sm:ml-6'>
+               <p className=' -ml-24 text-white text-xs '> Trafalgar provides progressive, and<br/>affordable healthcare, accessible on<br/> mobile and online 
+                 for everyone</p>
+               <div className=' mt-5 mb-3'>
+                 <p className='-ml-24 text-xs text-white'>©Trafalgar PTY LTD 2020. All rights reserved </p>
                </div>
             </div>
           </div>
@@ -51,8 +51,8 @@ function MyFooter() {
             </div>
           </div>
         </div>
-          <div className=' -ml-7 -mb-7'>
-            <img src={'../public/images/last e.svg'} />
+          <div className=' hidden sm:block -ml-7 -mb-7'>
+            <img src={'../images/last e.svg'} />
           </div>
       </div>
     </Footer>
